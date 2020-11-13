44c10c73365a2ddac3abef8e55a281ebe9a8b24f
Dan Hannam <rawritzdan@gmail.com>
2020-11-13 21:42:34 +0000

    Commit 2

--- build/root/install.sh
+++ build/root/install.sh
@@ -73,7 +73,7 @@ fi
 # curly.sh -of "/tmp/minecraft_server.jar" -url "${minecraft_java_url}"
 
 # # move minecraft java server
-# mkdir -p "/srv/minecraft" && mv "/tmp/minecraft_server.jar" "/srv/minecraft/"
+mkdir -p "/srv/minecraft" #&& mv "/tmp/minecraft_server.jar" "/srv/minecraft/"
 
 # container perms
 ####
